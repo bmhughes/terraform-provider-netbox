ef1a9ce34eb4a36986d673d05acd9a2d8ff255e9
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2021-04-05 20:07:37 +0100

    Add debug mode

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/smutel/terraform-provider-netbox/netbox"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
 			return netbox.Provider()
 		},
-	})
+	}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terrafor.io/smutel/netbox", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
